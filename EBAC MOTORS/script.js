f23b632b7d7392e167923146383e3262028de62b
SeraphCloud <alexandre.victor264@icloud.com>
2024-08-27 13:49:24 -0300

    Adicionadas as seções de destaque e promoções

--- EBAC MOTORS/script.js
+++ EBAC MOTORS/script.js
@@ -2,4 +2,8 @@ $(document).ready(function() {
     $('#carousel-images').slick({
         autoplay: true,
     })
+
+    $('.menu-hamburguer').on('click', function() {
+        $('nav').slideToggle();
+    })
 })
